c48fba058b6dbd8079aa5e343cf033e987d3caae
A Erwin S 阎 <147702750+aerwins-yyw@users.noreply.github.com>
2024-09-10 23:01:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,41 @@ with col1:
     st.image(profile_pic, width=230, use_column_width=False, output_format="auto")
 
 with col2:
+    st.markdown(
+        """
+        <style>
+        .main-title {
+            font-size: 36px; /* Adjust font size as needed */
+            white-space: nowrap; /* Prevent wrapping */
+            text-align: center; /* Center the text */
+        }
+        .sub-title {
+            font-size: 24px; /* Adjust the subtitle font size */
+            white-space: nowrap; /* Prevent wrapping */
+            text-align: center; /* Center the text */
+        }
+        .description {
+            font-size: 18px; /* Adjust the description font size */
+            text-align: left; /* Center the text */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
     st.markdown(f"<h1 class='main-title'>{NAME}</h1>", unsafe_allow_html=True)
     st.markdown(f"<h2 class='sub-title'>{姓名}</h2>", unsafe_allow_html=True)
     st.markdown(f"<p class='description'>{DESCRIPTION}</p>", unsafe_allow_html=True)
+# Three Main Principles in Professional Life
+st.write('\n')
+st.markdown(
+    """
+    <div style="text-align: left; margin-top: -10px; margin-bottom: 20px;">
+        <em>Sincerity, Grit, Innovation</em><br>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # Download Resume Button
 st.download_button(
@@ -89,10 +121,10 @@ st.markdown(f"""
 
 # --- SOCIAL MEDIA LINKS WITH ICONS ---
 SOCIAL_MEDIA_ICONS = {
-    "LinkedIn": r"C:/Users/HP/Documents/Digital CV/assets/LinkedIn Logo.webp",
-    "GitHub": r"C:/Users/HP/Documents/Digital CV/assets/GitHub Logo.png",
-    "Instagram": r"C:/Users/HP/Documents/Digital CV/assets/Instagram Logo.webp",
-    "X": r"C:/Users/HP/Documents/Digital CV/assets/X Logo.jpg"
+    "LinkedIn": "C:/Users/HP/Documents/Digital CV/assets/LinkedIn Logo.webp",
+    "GitHub": "C:/Users/HP/Documents/Digital CV/assets/GitHub Logo.png",
+    "Instagram": "C:/Users/HP/Documents/Digital CV/assets/Instagram Logo.webp",
+    "X": "C:/Users/HP/Documents/Digital CV/assets/X Logo.jpg"
 }
 
 # Display social media links with icons
@@ -113,9 +145,10 @@ st.write('\n')
 st.subheader("Experience & Qualifications")
 st.write("""
 - ✔️ 3 Years of Working Experience in Education, Research, and Government Relations
-- ✔️ Well-trained in writing and public speaking, understanding of statistical principles and applications
+- ✔️ Well-trained in writing and public speaking 
+- ✔️ Understanding of statistical principles and applications
 - ✔️ Excellent team player and displaying a strong sense of initiative on tasks
-- ✔️ Life-long Learner, with focus area in Science-Technology Innovation Policy, Smart City, and AI. 
+- ✔️ Life-long Learner, with focus in public policy, data and AI, enterpreneurship 
 """)
 
 # --- SKILLS ---
@@ -128,14 +161,14 @@ st.write("""
 - 🗄️ Databases: MySQL, IBM DB2
 """)
 
-# --- lANGUAGE ---
+# --- LANGUAGE ---
 st.write('\n')
 st.subheader("Language Proficiency")
 st.write("""
-- 👩‍💻 Indonesian: Native
-- 📊 English: Fluent (IELTS Band 8, TOEFL IBT 104/120)
-- 📚 Chinese: Advance (HSK 6)
-- 🗄️ French: Intermediate (DELF B2)
+- 🇮🇩 Indonesian: Native
+- 🇬🇧 English: Fluent (IELTS Band 8, TOEFL IBT 104/120)
+- 🇨🇳 Chinese: Advance (HSK 6)
+- 🇫🇷 French: Intermediate (DELF B2)
 """)
 
 # --- WORK HISTORY ---
@@ -143,9 +176,9 @@ st.write('\n')
 st.subheader("Work History")
 st.write("---")
 
-# --- JOB 1
-# (add logo Here)
-st.write("🚧", "**Data Analyst and Illutrator | KOMPAS Research & Development**")
+# --- JOB 1 ---
+st.image("C:/Users/HP/Documents/Digital CV/assets/Litbang Kompas Logo.png", width=200)  # Add logo here
+st.write("💡", "**Assistant Researcher, Data Analyst, Illustrator | KOMPAS Research & Development**")
 st.write("03/2020 - Present")
 st.write("""
 - ► Doing research on various fields, particularly Indonesia's industrial upstreaming policy, energy transition, AI and Big Data regulation. 
@@ -153,19 +186,31 @@ st.write("""
 - ► Research Report Illustrator
 """)
 
-# --- JOB 2
+# --- JOB 2 ---
 st.write('\n')
-st.write("🚧", "**Consultant | Coordinating Ministry for Maritime Affairs and Investment, Republic of Indonesia**")
+st.markdown(
+    """
+    <div style="display: flex; align-items: center;">
+        <img src='C:/Users/HP/Documents/Digital CV/assets/Kemenko Marves Logo.png' style='width:50px;'/>
+        <img src='C:/Users/HP/Documents/Digital CV/assets/hdcm Logo.png' style='width:50px; margin-right: 10px;'/>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
+
+st.write('\n')
+st.write("📊", "**Consultant | Coordinating Ministry for Maritime Affairs and Investment, Republic of Indonesia**")
 st.write("08/2021 - 09/2022")
 st.write("""
-- ► Consultant for Secretariate of High-level Dialogue and Cooperation Mechanism between Republic of Indonesia and People's Republic of China
+- ► Consultant for Secretariat of High-level Dialogue and Cooperation Mechanism between Republic of Indonesia and People's Republic of China
 - ► Coordinating inter-ministerial works and bilateral meetings, supervising investment projects, and hosting business matching forums and focus group discussions
-- ► Chinese interpreter and documents translations
+- ► Chinese interpreter and document translations
 """)
 
-# --- JOB 3
+# --- JOB 3 ---
 st.write('\n')
-st.write("🚧", "**Teacher | Xavier High School, Federated States of Micronesia**")
+st.image("C:/Users/HP/Documents/Digital CV/assets/Xavier High School Logo.png", width=50)  # Add logo here
+st.write("🇫🇲", "**Teacher | Xavier High School, Federated States of Micronesia**")
 st.write("08/2019 - 03/2020")
 st.write("""
 - ► Teaching Ethics, Latin Language, French A1 and Chinese HSK 1-2
@@ -182,7 +227,17 @@ st.write("🎓", "**Master of Public Administration**")
 st.write("Tsinghua University, Beijing, China | 2022 - Present")
 st.write("""
 - ► Focus Areas: Data Science, Policy Analysis, AI in Business Strategy
+- ► GPA: 3.91 (4.00)
 - ► Thesis: Impact of ASEAN-Smart City Network on Urban Developments 
+""")
+
+st.write('\n')
+st.write("🎓", "**Bachelor of Engineering**")
+st.write("Universitas Yuppentek Indonesia | 2022 - Present")
+st.write("""
+- ► Major: Electrical Engineering
+- ► GPA: 4.00 (4.00)
+- ► Thesis: -
 """)
 
 st.write('\n')
@@ -190,17 +245,20 @@ st.write("🎓", "**Bachelor of Arts**")
 st.write("Driyarkara School of Philosophy, Jakarta, Indonesia | 2015 - 2019")
 st.write("""
 - ► Major: Philosophy
+- ► GPA: 3.99 (4.00)
 - ► Thesis: Struggle for Recognition - Axel Honnet's Critical Theory
-- ► Extracurricular: Driyarkara Journal Coordinator in Chief & Head of Editors 2016-2017; MaGis Community Coordinator 2016-2018
+- ► Driyarkara Journal Coordinator in Chief & Head of Editors 2016-2017; MaGis Community Coordinator 2016-2018; Hermanum College International Students Tutor
 """)
 
 # Professional Training Section
 st.write('\n')
 st.subheader("Professional Training & Certifications")
 st.write("""
-- 🏅 **Certified Data Scientist (CDS)** – DataCamp | 2021
-- 🏅 **Advanced AI & Machine Learning for Business** – Coursera | 2020
-- 🏅 **Public Policy Design and Analysis** – HarvardX | 2019
+- 🏅 **Data Science with R Professional Certificate** – Harvard University
+- 🏅 **Data Science Professional Certificate** – IBM
+- 🏅 **Science and Technology Policy Course** – Massachusetts Institute of Technology
+- 🏅 **Project Management Professional Certificate** – University of Adelaide
+- 🏅 **French B2** - l’Institut Français d’Indonésie, Jakarta  
 """)
 
 # --- PROJECTS & ACCOMPLISHMENTS ---
